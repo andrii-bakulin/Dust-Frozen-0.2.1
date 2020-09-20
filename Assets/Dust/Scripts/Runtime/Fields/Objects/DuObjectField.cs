d18ea676881c6f8be80fc55e2b74529ef369fbd1
Andrii Bakulin <andrei.bakulin@gmail.com>
2020-09-20 18:05:54 +0700

    Change colors for fields gizmos

--- Assets/Dust/Scripts/Runtime/Fields/Objects/DuObjectField.cs
+++ Assets/Dust/Scripts/Runtime/Fields/Objects/DuObjectField.cs
@@ -5,8 +5,8 @@ namespace DustEngine
 {
     public abstract class DuObjectField : DuField
     {
-        protected static readonly Color k_GizmosColorRangeZero = new Color(0.33f, 0.0f, 0.0f);
-        protected static readonly Color k_GizmosColorRangeOne = new Color(0.80f, 0.0f, 0.0f);
+        protected static readonly Color k_GizmosColorRangeZero = new Color(0.00f, 0.50f, 0.66f);
+        protected static readonly Color k_GizmosColorRangeOne = new Color(0.00f, 0.75f, 1.00f);
 
         //--------------------------------------------------------------------------------------------------------------
 
@@ -70,7 +70,7 @@ namespace DustEngine
 
         protected Color GetGizmoColorRange0()
         {
-            return gizmoFieldColor ? remapping.color * 0.5f : k_GizmosColorRangeZero;
+            return gizmoFieldColor ? remapping.color * 0.66f : k_GizmosColorRangeZero;
         }
 
         protected Color GetGizmoColorRange1()
