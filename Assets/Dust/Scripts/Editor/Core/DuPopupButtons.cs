7ccdddd0935f16aa1a27446fcec1c08c290cec3e
Andrii Bakulin <andrei.bakulin@gmail.com>
2020-09-22 22:13:53 +0700

    Temporary drop DuStepObjectsField class & support data

--- Assets/Dust/Scripts/Editor/Core/DuPopupButtons.cs
+++ Assets/Dust/Scripts/Editor/Core/DuPopupButtons.cs
@@ -53,7 +53,6 @@ namespace DustEngine.DustEditor
                 popup.AddItem(column, typeof(DuConstantField), "Constant");
                 popup.AddItem(column, typeof(DuCoordinatesField), "Coordinates");
                 popup.AddItem(column, typeof(DuRadialField), "Radial");
-                popup.AddItem(column, typeof(DuStepObjectsField), "Step Objects");
                 popup.AddItem(column, typeof(DuTimeField), "Time");
 
                 popup.m_ColumnRecords.Add(column);
