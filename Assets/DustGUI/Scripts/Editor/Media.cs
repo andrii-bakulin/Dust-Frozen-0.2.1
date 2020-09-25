93c41929cebfbd7171c8465e7d730f68cc358f61
Andrii Bakulin <andrei.bakulin@gmail.com>
2020-09-25 15:34:07 +0700

    Improve DustGUI

--- Assets/DustGUI/Scripts/Editor/Media.cs
+++ Assets/DustGUI/Scripts/Editor/Media.cs
@@ -9,5 +9,46 @@ namespace DustEngine
         {
             EditorGUI.LabelField(rect, new GUIContent(texture));
         }
+
+        public static void Gradient(Rect rect, Gradient gradient)
+        {
+            EditorGUI.LabelField(rect, new GUIContent(CreateGradientTexture(gradient, rect.width, rect.height)));
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public static Texture2D CreateGradientTexture(Gradient gradient, float width, float height)
+            => CreateGradientTexture(gradient, (int) width, (int) height);
+
+        public static Texture2D CreateGradientTexture(Gradient gradient, int width, int height)
+        {
+            if (width < 0)
+                width = 8;
+
+            if (height < 0)
+                height = 8;
+
+            var texture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+
+            texture.hideFlags = HideFlags.HideAndDontSave;
+
+            Color[] pixels = new Color[width * height];
+
+            for (int x = 0; x < width; x++)
+            {
+                Color color = gradient.Evaluate((float) x / (width - 1));
+
+                for (int y = 0; y < height; y++)
+                {
+                    pixels[y * width + x] = color;
+                }
+            }
+
+            texture.SetPixels(pixels);
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.Apply();
+
+            return texture;
+        }
     }
 }
