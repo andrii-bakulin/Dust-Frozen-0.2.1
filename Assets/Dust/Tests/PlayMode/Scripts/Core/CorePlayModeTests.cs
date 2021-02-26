b2af0c04e72db06b5a9a377154190622879de031
Andrii Bakulin <andrii.bakulin@gmail.com>
2021-02-26 14:05:31 +0700

    Tests: Improve Assert logic for Vector3

--- Assets/Dust/Tests/PlayMode/Scripts/Core/CorePlayModeTests.cs
+++ Assets/Dust/Tests/PlayMode/Scripts/Core/CorePlayModeTests.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 public abstract class CorePlayModeTests
 {
     protected readonly float TIME_SCALE = 0.25f;
-    protected readonly string FLOAT_ACCURACY_MASK = "F2"; // @todo: F5
+
+    protected readonly float VECTOR_DISTANCE_DELTA = 0.001f;
+    protected readonly string FLOAT_ACCURACY_MASK = "F3";
 
     protected float Sec(float sec)
     {
         return sec * TIME_SCALE;
     }
 
-    protected void Assert_Equal(Vector3 vut, Vector3 baseValue, string message = "")
+    protected void Assert_Equal(Vector3 testValue, Vector3 baseValue, string message = "")
     {
-        Assert.That(vut.ToString(FLOAT_ACCURACY_MASK), Is.EqualTo(baseValue.ToString(FLOAT_ACCURACY_MASK)), message);
+        float distance = Vector3.Distance(testValue, baseValue);
+
+        message += (string.IsNullOrEmpty(message) ? "" : "\n" ) +
+                   $"Expected: Vector3({baseValue.ToString("F3")})\n" +
+                   $"But was:  Vector3({testValue.ToString("F3")})\n" +
+                   $"Distance: {distance} is greater than delta {VECTOR_DISTANCE_DELTA}";
+        
+        Assert.That(distance, Is.LessThanOrEqualTo(VECTOR_DISTANCE_DELTA), message);
     }
 
-    protected void Assert_NotEqual(Vector3 vut, Vector3 baseValue, string message = "")
+    protected void Assert_NotEqual(Vector3 testValue, Vector3 baseValue, string message = "")
     {
-        Assert.That(vut.ToString(FLOAT_ACCURACY_MASK), Is.Not.EqualTo(baseValue.ToString(FLOAT_ACCURACY_MASK)), message);
+        float distance = Vector3.Distance(testValue, baseValue);
+
+        message += (string.IsNullOrEmpty(message) ? "" : "\n" ) +
+                  $"Expected: Vector3({baseValue.ToString("F3")})\n" +
+                  $"But was:  Vector3({testValue.ToString("F3")})\n" +
+                  $"Distance: {distance} is less than delta {VECTOR_DISTANCE_DELTA} so vectors are EQUAL";
+        
+        Assert.That(distance, Is.Not.LessThanOrEqualTo(VECTOR_DISTANCE_DELTA), message);
     }
 }
